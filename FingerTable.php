34c2d94051de777e3308f7ee61595db07d527f18
JSON <ClosetMonkey@users.noreply.github.com>
2019-08-23 01:21:48 -0700

    Update FingerTable.php

--- FingerTable.php
+++ FingerTable.php
@@ -2,22 +2,22 @@
 
 class FingerTable {
 
-	public $fingers = []; // 
+  public $fingers = []; // 
 
-	function __construct($node) {
-		$this->fingers = [];
+  function __construct($node) {
+    $this->fingers = [];
 
-		for ($i = 0; $i < KEYSIZE; $i++) {
-			$start = $node->getNodeKey()->createStartKey($i);
-			$this->fingers[$i] = new Finger($start, $node);
-		}
-	}
+    for ($i = 0; $i < KEYSIZE; $i++) {
+      $start = $node->getNodeKey()->createStartKey($i);
+      $this->fingers[$i] = new Finger($start, $node);
+    }
+  }
 
-	function getFinger($i) {
-		return $this->fingers[$i];
-	}
+  function getFinger($i) {
+    return $this->fingers[$i];
+  }
 
-	function size() {
-		return count($this->fingers);
-	}
+  function size() {
+    return count($this->fingers);
+  }
 }
