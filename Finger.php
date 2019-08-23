1662c0bdc7d5595d61652e82b23eb0344bfb0f44
JSON <ClosetMonkey@users.noreply.github.com>
2019-08-23 01:19:10 -0700

    Fix tabs etc

--- Finger.php
+++ Finger.php
@@ -2,28 +2,28 @@
 
 class Finger {
 
-	public $start;
-	public $node;
+  public $start;
+  public $node;
 
-	function __construct($start, $node) {
-		$this->node = $node;
-		$this->start = $start;
-	}
+  function __construct($start, $node) {
+    $this->node = $node;
+    $this->start = $start;
+  }
 
-	function getStart() {
-		return $this->start;
-	}
+  function getStart() {
+    return $this->start;
+  }
 
-	function setStart($start) {
-		$this->start = $start;
-	}
+  function setStart($start) {
+    $this->start = $start;
+  }
 
-	function getNode() {
-		return $this->node;
-	}
+  function getNode() {
+    return $this->node;
+  }
 
-	function setNode($node) {
-		$this->node = $node;
-	}
+  function setNode($node) {
+    $this->node = $node;
+  }
 
 }
