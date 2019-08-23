837489048ba2b7d9e2347902ae6b9b2ee36b8216
JSON <ClosetMonkey@users.noreply.github.com>
2019-08-23 01:22:30 -0700

    Update Hash.php

--- Hash.php
+++ Hash.php
@@ -3,37 +3,36 @@
 
 class Hash {
 
-	public $function = "RAND";
-	public $keylen = 4;
+  public $function = "RAND";
+  public $keylen = 4;
 
-	function hash($identifier) {
-		$this->keylen = KEYSIZE;
-		return rand(0, pow(2, KEYSIZE )-1);
-	}
+  function hash($identifier) {
+    $this->keylen = KEYSIZE;
+    return rand(0, pow(2, KEYSIZE )-1);
+  }
 
-	function getFunction() {
-		return $this->function;
-	}
+  function getFunction() {
+    return $this->function;
+  }
 
-	function setFunction($function) {
-		if ($function == "SHA-1") {
-			$this->keylen = 160;
-		}
-		if ($function == "CRC32") {
-			$this->keylen = 32;
-		}
-		if ($function == "RAND") {
-			$this->keylen = 32;
-		}
-		$this->function = $function;
-	}
+  function setFunction($function) {
+    if ($function == "SHA-1") {
+      $this->keylen = 160;
+    }
+    if ($function == "CRC32") {
+      $this->keylen = 32;
+    }
+    if ($function == "RAND") {
+      $this->keylen = 32;
+    }
+    $this->function = $function;
+  }
 
-	function getKeyLength() {
-		return $this->keylen;
-	}
+  function getKeyLength() {
+    return $this->keylen;
+  }
 
-	function setKeyLength($keyLength) {
-		$this->keylen = $keyLength;
-	}
-
+  function setKeyLength($keyLength) {
+    $this->keylen = $keyLength;
+  }
 }
